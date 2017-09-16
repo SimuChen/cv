2816f11a15afaf4ea8a010527a86321b59521a52
SimuChen <31510573+SimuChen@users.noreply.github.com>
2017-09-16 21:15:22 +0800

    Update mycv.js

--- js/mycv.js
+++ js/mycv.js
@@ -3,18 +3,7 @@
  */
 
 
-//鑳屾櫙闅忔満
 $(function() {
-    var length = 3;
-    $(".bg-img li:nth-child(2)").show();
-    setInterval(function () {
-        var randomBgIndex = Math.round(Math.random() * length);
-        $("#section1 .bg-img li").eq(randomBgIndex).addClass("show").siblings().removeClass("show");
-    },5000);
-});
-
-$(function() {
-    //缂栬緫鍣ㄦ帶鍒�
     $("h4,.nav b").css("color","#fff");
 
     $(".fades").addClass("fadesin");
@@ -30,59 +19,33 @@ $(function() {
     var tops = [top1,top2,top3,top4];
     $(window).resize();
 
-    //鍥炲埌椤堕儴
-    $("#top").click(function () {
-        $('html,body').stop().animate({
-            scrollTop: 0
-        }, 700);
-    });
-    showScroll();
-    var min_height = document.documentElement.clientHeight /2;
-    function showScroll() {
-        $(window).scroll(function() {
-            var s = $(window).scrollTop();
-            s > min_height ? $('#top,#down').fadeIn() : $('#top,#down').fadeOut();
-        });
-    }
-    //瀵艰埅楂樹寒
+    
+    //导航高亮
     $(".nav-ul li").bind("click", function () {
-        var index = $(this).index();//鑾峰彇搴忓彿
+        var index = $(this).index();//获取序号
         $(".nav-ul li").eq(index).addClass("active").siblings().removeClass("active");
         $(".nav-xs-ul li").eq(index).addClass("active").siblings().removeClass("active");
     });
-    //鍏充簬鎴�
+    //关于我
     $("#abMe").bind("click", function () {
        $(".nav-ul li:nth-child(1)").click();
     });
-    //鎴戠殑浣滃搧
-    $("#mypro").bind("click", function () {
-        $(".nav-ul li:nth-child(3)").click();
-    });
-    //瀵艰埅鐐瑰嚮
+    //导航点击
     $(".nav-ul li").bind("click", function () {
-        var index = $(this).index();//鑾峰彇搴忓彿
-        _top = $(".section").eq(index+1).offset().top; //鑾峰彇瀵瑰簲div璺濋《楂樺害
+        var index = $(this).index();//获取序号
+        _top = $(".section").eq(index+1).offset().top; //获取导航对应div距顶高度
         moveTo();
     });
-    //瀵艰埅slideToggle
-    $(".more-nav").bind("click", function () {
-        $(".nav-ul.nav-xs-ul").stop().slideToggle(300);
-    });
-    function moveTo(){
-        $('html,body').animate({
-            scrollTop: _top
-        }, 500);
-    }
     $(".nav-xs-ul li").click(function () {
        $(".nav-xs-ul").slideUp(300)
     });
 
-    //椤甸潰婊氬姩
+    //页面滚动
     $(window).scroll(function () {
-        //瀵艰埅fixed
+        //导航fixed
         var s = $(window).scrollTop();
         s > top1 ? $('#nav-bar').addClass("fixed") : $('#nav-bar').removeClass("fixed");
-        //瀵艰埅璺熼殢婊氬姩鍝嶅簲
+        //导航跟随滚动响应
         if((s>top1)&&(s<top2)){
             $(".nav-ul li").eq(0).addClass("active").siblings().removeClass("active");
             $(".nav-xs-ul li").eq(0).addClass("active").siblings().removeClass("active");
